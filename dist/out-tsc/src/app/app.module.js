75d441d18c6a681038302bb4ec9054ede3c4c8da
Dickson <dicksonparlma@gmail.com>
2019-03-20 17:13:11 +0300

    Goals files uploaded

---
+++ dist/out-tsc/src/app/app.module.js
@@ -0,0 +1,29 @@
+import * as tslib_1 from "tslib";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = tslib_1.__decorate([
+        NgModule({
+            declarations: [
+                AppComponent,
+                GoalComponent,
+                GoalDetailsComponent
+            ],
+            imports: [
+                BrowserModule,
+                AppRoutingModule
+            ],
+            providers: [],
+            bootstrap: [AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
---
+++ dist/out-tsc/src/app/goal.js
@@ -0,0 +1,11 @@
+var Goal = /** @class */ (function () {
+    function Goal(id, name, description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.showDescription = false;
+    }
+    return Goal;
+}());
+export { Goal };
+//# sourceMappingURL=goal.js.map
